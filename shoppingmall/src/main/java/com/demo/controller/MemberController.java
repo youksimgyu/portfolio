e06d87264f117798752e0966231a7bf80b119e56
youksimgyu <dbrtlarb@gmail.com>
2022-07-11 10:54:22 +0900

    jsp 추가

--- shoppingmall/src/main/java/com/demo/controller/MemberController.java
+++ shoppingmall/src/main/java/com/demo/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
@@ -10,4 +11,9 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/member/*")
 public class MemberController {
 
+	@GetMapping("/join")
+	public void join() {
+		
+	}
+	
 }
