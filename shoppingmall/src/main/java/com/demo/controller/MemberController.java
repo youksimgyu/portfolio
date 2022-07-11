4034177e64aa409d4a1bb90d5d7d93c93e545126
youksimgyu <dbrtlarb@gmail.com>
2022-07-11 13:54:48 +0900

    로그인 연결

--- shoppingmall/src/main/java/com/demo/controller/MemberController.java
+++ shoppingmall/src/main/java/com/demo/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.demo.domain.MemberVO;
+import com.demo.service.MemberService;
 
 import lombok.extern.log4j.Log4j;
 
@@ -15,6 +17,9 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/member/*")
 public class MemberController {
 
+	@Autowired
+	private MemberService service;
+	
 	@GetMapping("/join")
 	public void join() {
 		
@@ -28,6 +33,8 @@ public class MemberController {
 		}
 		
 		log.info(vo); // vo.toString()
+		
+		service.join(vo);
 		
 		return "";
 	}
